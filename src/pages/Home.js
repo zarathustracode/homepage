047a36bc02b6de46c4b61f82414771d8fcf75bdb
Nebojsa Gasparovic <nebojsa.gasparovic@bcf.uni-freiburg.de>
2022-05-31 16:14:03 +0200

    |Adding features section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import HeroSection from "../components/HeroSection"
 import Cards from "../components/Cards"
 import Footer from "../components/Footer"
+import Features from "../components/Features"
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <Features />
       <Cards title='Check out my portfolio' />
       <Footer />
     </>
